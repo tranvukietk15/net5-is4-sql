30344b4c2993b2ecda90b98f480b26603bfd39d0
janos <janos.tran@transwap.com>
2021-09-12 22:55:38 +0700

    add admin

---
+++ net5-is4admin/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace net5_is4admin.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
